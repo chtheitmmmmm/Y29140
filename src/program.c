83c3af76bfab181671cdb3b3cf1f56512226c627
Cmtheit <cmtheit@192.168.0.102>
2023-05-23 00:26:27 +0800

    deli

--- src/program.c
+++ src/program.c
@@ -20,6 +20,8 @@
 
 #define BS_NOT_FOUND (-1)					  /* used by binary search */
 #define BS_FOUND 0
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
 
 typedef int data_t; 				  		  /* data type */
 
@@ -35,6 +37,9 @@ void quick_sort(data_t dataset[], int n);
 int binary_search(data_t dataset[], int lo, int hi, 
 	data_t *key, int *locn);
 
+void binary_search_range(data_t dataset[], int lo, int hi,
+		data_t *key, int *locn);
+
 void stage_one();
 void stage_two();
 void stage_three();
@@ -43,17 +48,21 @@ void stage_four();
 /* add your own function prototypes here */
 
 /****************************************************************/
+data_t dataset[DATASET_SIZE];
+int max_err;
+
+struct function {
+    int a;
+    int b;
+    int max_element;
+};
+
+int functions_num = 0;
+struct function all_functions[DATASET_SIZE];
 
 /* main function controls all the action */
 int
 main(int argc, char *argv[]) {
-	/* to hold all input data */
-	data_t dataset[DATASET_SIZE];
-	int max_err;
-
-	/* to hold the mapping functions */
-
-
 	/* stage 1: read and sort the input */
 	stage_one(); 
 	
@@ -82,18 +91,61 @@ stage_one() {
 	/* add code for stage 1 */
 	/* print stage header */
 	print_stage_header(STAGE_NUM_ONE);
-
-	
+    for (int i = 0; i < DATASET_SIZE; ++i) {
+        scanf("%d", dataset + i);
+    }
+    quick_sort(dataset, DATASET_SIZE);
+    printf("First %d numbers:", DATA_OUTPUT_SIZE);
+    for (int i = 0; i < DATA_OUTPUT_SIZE; ++i) {
+        printf(" %d", dataset[i]);
+    }
+    printf("\n");
 	printf("\n");
 }
 
 /* stage 2: compute the first mapping function */
-void 
+int
+f(int a, int b, int key) {
+    return b ? (int)ceil((key + a) * 1.0 / b) : a;
+}
+
+static inline void
+construct_function(int i, struct function * func) {
+    if (i + 1 >= DATASET_SIZE) {
+        func->a = DATASET_SIZE - 1;
+        func->b = 0;
+        func->max_element = dataset[i];
+    } else {
+        if (dataset[i] != dataset[i + 1]) {
+            func->a = -(dataset[i] - (dataset[i + 1] - dataset[i]) * i);
+            func->b = dataset[i + 1] - dataset[i];
+        } else {
+            func->a = i;
+            func->b = 0;
+        }
+    }
+}
+
+void
 stage_two() {
 	/* add code for stage 2 */
 	/* print stage header */
 	print_stage_header(STAGE_NUM_TWO);
-
+    const int a = -dataset[0];
+    const int b = dataset[1] - dataset[0];
+    int max_error = 0;
+    int max_error_idx = 0;
+    for (int i = 1; i < DATASET_SIZE; ++i) {
+        int prediction = f(a, b, dataset[i]);
+        int the_error = abs(i - prediction);
+        if (the_error > max_error) {
+            max_error = the_error;
+            max_error_idx = i;
+        }
+    }
+    printf("Maximum prediction error: %d\n", max_error);
+    printf("For key: %d\n", dataset[max_error_idx]);
+    printf("At position: %d\n", max_error_idx);
 	printf("\n");
 }
 
@@ -103,16 +155,69 @@ stage_three() {
 	/* add code for stage 3 */
 	/* print stage header */
 	print_stage_header(STAGE_NUM_THREE);
-
+    scanf("%d", &max_err);
+    construct_function(0, all_functions + functions_num);
+    functions_num++;
+    struct function * current_function = all_functions + functions_num - 1;
+    for (int i = 2; i < DATASET_SIZE; ++i) {
+        if (i + 1 < DATASET_SIZE) {
+            int prediction = f(current_function->a, current_function->b, dataset[i]);
+            int the_error = abs(i - prediction);
+            if (the_error > max_err) {
+                current_function->max_element = dataset[i - 1];
+                construct_function(i, all_functions + functions_num);
+                current_function = all_functions + functions_num;
+                functions_num++;
+                i += 1;
+            }
+        } else {
+            current_function->max_element = dataset[i];
+        }
+    }
+    printf("\n");
+    for (int i = 0; i < functions_num; ++i) {
+        printf("Function %2d: a = %4d, b = %4d, max element = %3d\n",
+               i, all_functions[i].a, all_functions[i].b, all_functions[i].max_element);
+    }
 	printf("\n");
 }
 
 /* stage 4: perform exact-match queries */
 void stage_four() {
 	/* add code for stage 4 */
-	/* print stage header */
+	/* print stage header   */
 	print_stage_header(STAGE_NUM_FOUR);
-	
+    int key;
+    while (scanf("%d", &key) != EOF) {
+        printf("Searching for %d:\n", key);
+        printf("Step 1: ");
+        if (key < dataset[0] || key > dataset[DATASET_SIZE - 1]) {
+            printf("not found!\n");
+            continue;
+        } else {
+            printf("search key in data domain.\n");
+        }
+        printf("Step 2: ");
+        int max_elements[DATASET_SIZE];
+        for (int i = 0; i < functions_num; ++i) {
+            max_elements[i] = all_functions[i].max_element;
+        }
+        int seg_idx;
+        binary_search_range(max_elements, 0, functions_num, &key, &seg_idx);
+        printf("\n");
+        printf("Step 3: ");
+        struct function the_func = all_functions[seg_idx];
+        int the_prediction = f(the_func.a, the_func.b, key);
+        int the_loc;
+        if (binary_search(dataset,
+                          MAX(0, the_prediction - max_err),
+                          MIN(DATASET_SIZE, the_prediction + max_err + 1), &key, &the_loc) == BS_FOUND) {
+            printf("@ dataset[%d]!", the_loc);
+        } else {
+            printf("not found!");
+        }
+        printf("\n");
+    }
 	printf("\n");
 }
 
@@ -199,7 +304,7 @@ binary_search(data_t dataset[], int lo, int hi,
 		return BS_NOT_FOUND;
 	}
 	mid = (lo+hi)/2;
-	
+	printf("%d ", dataset[mid]);    // output to stdout
 	if ((outcome = cmp(key, dataset+mid)) < 0) {
 		return binary_search(dataset, lo, mid, key, locn);
 	} else if (outcome > 0) {
@@ -207,5 +312,24 @@ binary_search(data_t dataset[], int lo, int hi,
 	} else {
 		*locn = mid;
 		return BS_FOUND;
+	}
+}
+
+void binary_search_range(data_t dataset[], int lo, int hi,
+		data_t *key, int *locn) {
+	int mid, outcome;
+	/* if key is in dataset, it is between dataset[lo] and dataset[hi-1] */
+	if (lo>=hi) {
+		*locn = lo;
+        return;
+	}
+	mid = (lo+hi)/2;
+	printf("%d ", dataset[mid]);    // output to stdout
+	if ((outcome = cmp(key, dataset+mid)) < 0) {
+		binary_search_range(dataset, lo, mid, key, locn);
+	} else if (outcome > 0) {
+		binary_search_range(dataset, mid+1, hi, key, locn);
+	} else {
+		*locn = mid;
 	}
 }
